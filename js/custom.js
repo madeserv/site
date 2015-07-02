e88e39ac902c741fc98dc46e5f861530ac74ddc6
RenatoScarpari <renatoscarpari@yahoo.com.br>
2015-07-01 21:38:16 -0300

    Contact validation field fixes

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,6 @@
   /* ---------------------------------------------- /*
    * E-mail validation
   /* ---------------------------------------------- */
-
   function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     return pattern.test(emailAddress);
@@ -24,9 +23,24 @@
     var c_message = $('#message').val();
     var response = $('#contact-form .ajax-response');
 
-    if (( c_name== '' || c_email == '' || c_city== '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
+    if (c_name.trim() == '') {
       response.fadeIn(500);
-      response.html('<i class="fa fa-warning"></i> Por favor, arrume os erros e tente novamente.');
+      response.html('<div class="text-center error"><i class="fa fa-warning"></i> Informe seu nome.</div>');
+    } else if (c_name.trim().length < 3) {
+      response.fadeIn(500);
+      response.html('<div class="text-center error"><i class="fa fa-warning"></i> O nome deve ter pelo menos 3 letras.</div>');
+    } else if (c_email.trim() == '') {
+      response.fadeIn(500);
+      response.html('<div class="text-center error"><i class="fa fa-warning"></i> Informe seu email.</div>');
+    } else if (!isValidEmailAddress(c_email)) {
+      response.fadeIn(500);
+      response.html('<div class="text-center error"><i class="fa fa-warning"></i> Email inválido.</div>');
+    } else if (c_message.trim() == '') {
+      response.fadeIn(500);
+      response.html('<div class="text-center error"><i class="fa fa-warning"></i> Escreva sua mensagem.</div>');
+    } else if (c_message.trim().length > 1000) {
+        response.fadeIn(500);
+        response.html('<div class="text-center error"><i class="fa fa-warning"></i> Só é permitido até mil letras na mensagem.</div>');
     } else {
       //$('#contact-form .ajax-hidden').ajax({url: 'https://docs.google.com/forms/d/1wjTYT6VUU6HM77z_eiRH1FDswrMhf4TEyRbo4o6PAwI/formResponse', type: 'POST'}).fadeOut(500);
       $('#contact-form .ajax-hidden').fadeOut(500);
@@ -37,10 +51,10 @@
         dataType: "xml",
         statusCode: {
           0: function (){
-               response.html("<p class=\"sent\">Sua mensagem foi enviada. <br /> Entraremos em contato o mais rápido possível. <br /> Obrigado.</p>").fadeIn(500);
+               response.html('<div class="text-center"><p class="sent">Sua mensagem foi enviada. <br /> Entraremos em contato o mais rápido possível. <br /> Obrigado.</p></div>').fadeIn(500);
              },
           200: function (){
-                 response.html("<p class=\"sent\">Sua mensagem foi enviada. <br /> Entraremos em contato o mais rápido possível. <br /> Obrigado.</p>").fadeIn(500);
+                 response.html('<div class="text-center"><p class="sent">Sua mensagem foi enviada. <br /> Entraremos em contato o mais rápido possível. <br /> Obrigado.</p></div>').fadeIn(500);
              }
         }
       });
